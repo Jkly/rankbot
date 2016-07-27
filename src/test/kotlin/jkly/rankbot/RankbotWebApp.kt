46156dc98cb3ecf8eee7bc65cab546e75856be60
Jkly <Jkly@users.noreply.github.com>
2016-07-27 17:46:02 +0100

    use spring boot starter for websockets to get all dependencies add skeleton for the slack handler

--- src/test/kotlin/jkly/rankbot/RankbotWebApp.kt
+++ src/test/kotlin/jkly/rankbot/RankbotWebApp.kt
@@ -1,10 +1,20 @@
 package jkly.rankbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
 
 @SpringBootApplication
 @EnableWebSocket
-open class RankbotWebApp {
+open class RankbotWebApp : WebSocketConfigurer {
 
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry.addHandler(slackHandler(), "/slack").addInterceptors(HttpSessionHandshakeInterceptor())
+    }
+
+    @Bean
+    open fun slackHandler() = SlackHandler()
 }
---
+++ src/test/kotlin/jkly/rankbot/SlackHandler.kt
@@ -0,0 +1,7 @@
+package jkly.rankbot
+
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+class SlackHandler: TextWebSocketHandler() {
+
+}
