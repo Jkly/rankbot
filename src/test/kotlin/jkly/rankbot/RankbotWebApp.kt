97763816fc27801e280fd064d74ef304b99bff03
Jkly <Jkly@users.noreply.github.com>
2016-07-27 17:17:16 +0100

    add spring websockets

--- src/test/kotlin/jkly/rankbot/RankbotWebApp.kt
+++ src/test/kotlin/jkly/rankbot/RankbotWebApp.kt
@@ -1,8 +1,10 @@
 package jkly.rankbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 
 @SpringBootApplication
+@EnableWebSocket
 open class RankbotWebApp {
 
 }
